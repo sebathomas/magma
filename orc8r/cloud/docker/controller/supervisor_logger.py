d7ec8765ec72828d8bb602ad0316b04b85729ff3
Sebastian Thomas <sebastian.thomas@tngtech.com>
2022-11-22 14:41:36 +0100

    WIP: Finish Ubuntu upgrade
    Signed-off-by: Sebastian Thomas <sebastian.thomas@tngtech.com>

--- orc8r/cloud/docker/controller/supervisor_logger.py
+++ orc8r/cloud/docker/controller/supervisor_logger.py
@@ -31,29 +31,37 @@ def write_stderr(s):
 
 def main():
     while 1:
-        # transition from ACKNOWLEDGED to READY
-        write_stdout('READY\n')
+        # # transition from ACKNOWLEDGED to READY
+        # write_stdout('READY\n')
+        #
+        # # read header line
+        # line = sys.stdin.readline()
+        #
+        # # read event payload
+        # headers = dict(x.split(':') for x in line.split())
+        # data = sys.stdin.read(int(headers['len']))
+        # write_stderr(data)
+        #
+        # # transition from READY to ACKNOWLEDGED
+        # write_stdout('RESULT 2\nOK')
+        write_stdout('READY\n') # transition from ACKNOWLEDGED to READY
+        line = sys.stdin.readline()  # read header line from stdin
+        write_stderr(line) # print it out to stderr
+        headers = dict([ x.split(':') for x in line.split() ])
+        data = sys.stdin.read(int(headers['len'])) # read the event payload
+        write_stderr(data) # print the event payload to stderr
+        write_stdout('RESULT 2\nOK') # transition from READY to ACKNOWLEDGED
 
-        # read header line
-        line = sys.stdin.readline()
 
-        # read event payload
-        headers = dict(x.split(':') for x in line.split())
-        data = sys.stdin.read(int(headers['len']))
-
-        # transition from READY to ACKNOWLEDGED
-        write_stdout('RESULT %s\n%s' % (len(data), data))
-
-
-def result_handler(event, response):
-    # Parse the headers
-    line, data = response.split('\n', 1)
-    headers = dict(x.split(':') for x in line.split())
-
-    # Get the log lines and prefix the process name and stdout/stderr
-    lines = data.rstrip().split('\n')
-    prefix = '%s %s | ' % (headers['processname'], headers['channel'])
-    print('\n'.join([prefix + l for l in lines]))
+# def result_handler(event, response):
+#     # Parse the headers
+#     line, data = response.split('\n', 1)
+#     headers = dict(x.split(':') for x in line.split())
+#
+#     # Get the log lines and prefix the process name and stdout/stderr
+#     lines = data.rstrip().split('\n')
+#     prefix = '%s %s | ' % (headers['processname'], headers['channel'])
+#     write_stderr('\n'.join([prefix + l for l in lines]))
 
 
 if __name__ == '__main__':
